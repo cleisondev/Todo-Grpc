39beea2b2d796dc2123aa60cbb27c9bd8c46aa98
Cleison Nunes <cleison.nunes20@gmail.com>
2025-05-20 23:48:15 -0300

    Initial commit

---
+++ TodoGrpc/Program.cs
@@ -0,0 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using TodoGrpc.Data;
+using TodoGrpc.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddGrpc();
+
+var app = builder.Build();
+
+app.MapGrpcService<GreeterService>();
+app.MapGrpcService<TodosService>();
+
+app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+
+app.Run();
---
+++ TodoGrpc/Services/TodosService.cs
@@ -0,0 +1,38 @@
+﻿using Grpc.Core;
+using TodoGrpc;
+using TodoGrpc.Data;
+using TodoGrpc.Models;
+
+namespace TodoGrpc.Services
+{
+    public class TodosService : Todoit.TodoitBase
+    {
+        private readonly AppDbContext _dbContext;
+
+        public TodosService(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public override Task<CreateTodoResponse> CreateToDo(CreateToDoRequest request, ServerCallContext context)
+        {
+            if(request.Title == string.Empty || request.Description == string.Empty)
+                throw new RpcException(new Status(StatusCode.InvalidArgument, "Title and Description cannot be empty"));
+
+
+            var todoItem = new ToDoItem
+            {
+                Title = request.Title,
+                Description = request.Description
+            };
+
+            _dbContext.AddAsync(todoItem);
+            _dbContext.SaveChangesAsync();
+
+            return Task.FromResult(new CreateTodoResponse
+            {
+                Id = todoItem.Id
+            });
+        }
+    }
+}
